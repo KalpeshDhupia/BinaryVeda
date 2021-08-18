38e5bf4704b9fb915a9108d6e663d1a68fa2bc32
Kalpesh Dhupia <kalpeshdhupia@gmail.com>
2021-08-18 11:09:24 +0530

    Second Screen

--- app/src/main/java/com/example/binaryveda/MainActivity.kt
+++ app/src/main/java/com/example/binaryveda/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.binaryveda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btnRegister.setOnClickListener {
+            val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
